494a7c5d03a0ef5d8508719b5b4a6cd4366d7e33
Mariah Cox <imariah77@gmail.com>
2022-02-11 14:19:25 -0800

    Add files via upload
    I was just messing around with the OS voices and decided to have some of the voices read out my favorite scene from The Princess Bride. Modify this file as you wish!

---
+++ talk2.py
@@ -0,0 +1,41 @@
+import os
+
+#os.system('say -v Daniel "xxxxxxxxxx"')
+#os.system('say -v Alex "xxxxxxxxxx"')
+#os.system('say -v Veena "xxxxxxxxxx"')
+
+#start here
+
+os.system('say -v Daniel "All right. Where is the poison? The battle of wits has begun. It ends when you decide and we both drink, and find out who is right... and who is dead."')
+os.system('say -v Alex "But its so simple. All I have to do is divine from what I know of you: are you the sort of man who would put the poison into his own goblet or his enemys? Now, a clever man would put the poison into his own goblet, because he would know that only a great fool would reach for what he was given. I am not a great fool, so I can clearly not choose the wine in front of you. But you must have known I was not a great fool, you would have counted on it, so I can clearly not choose the wine in front of me."')
+
+os.system('say -v Daniel "You\'ve made your decision then?"')
+os.system('say -v Alex "Not remotely. Because iocane comes from Australia, as everyone knows, and Australia is entirely peopled with criminals, and criminals are used to having people not trust them, as you are not trusted by me, so I can clearly not choose the wine in front of you."')
+
+os.system('say -v Daniel "Truly, you have a dizzying intellect."')
+os.system('say -v Alex "Wait till I get going! Now, where was I?"')
+
+os.system('say -v Daniel "Australia"')
+os.system('say -v Alex "Yes, Australia. And you must have suspected I would have known the powder\'s origin, so I can clearly not choose the wine in front of me."')
+
+os.system('say -v Daniel "You\'re just stalling now"')
+os.system('say -v Alex "You\'d like to think that, wouldn\'t you? You\'ve beaten my giant, which means you\'re exceptionally strong, so you could\'ve put the poison in your own goblet, trusting on your strength to save you, so I can clearly not choose the wine in front of you. But, you\'ve also bested my Spaniard, which means you must have studied, and in studying you must have learned that man is mortal, so you would have put the poison as far from yourself as possible, so I can clearly not choose the wine in front of me."')
+
+os.system('say -v Daniel "You\'re trying to trick me into giving away something. It won\'t work."')
+os.system('say -v Alex "It HAS WORKED! YOU\'VE GIVEN EVERYTHING AWAY! I KNOW WHERE THE POISON IS!"')
+
+os.system('say -v Daniel "Then make your choice."')
+os.system('say -v Alex "I will, and I choose... what in the world can that be?"')
+
+os.system('say -v Daniel "What? Where? I don\'t see anything."')
+os.system('say -v Alex "Well, I. I could have sworn I saw something. But no matter."')
+
+os.system('say -v Daniel "What\'s so funny?"')
+os.system('say -v Alex "I\'ll tell you in a minute. First, let\'s drink. Me from my glass, and you from yours."')
+
+os.system('say -v Daniel "You guessed wrong."')
+os.system('say -v Alex "You only think I guessed wrong! That\'s what\'s so funny! I switched glasses when your back was turned! Ha ha! You fool! You fell victim to one of the classic blunders - the most famous of which is never get involved in a land war in Asia. but only slightly less well-known is this: Never go in against a Sicilian when death is on the line! Ha ha ha ha ha ha ha! Ha ha ha ha ha ha ha! Ha ha ha..."')
+
+os.system('say -v Daniel "You all right?"')
+os.system('say -v Veena "And to think, all that time it was your cup that was poisoned."')
+os.system('say -v Daniel "They were both poisoned. I spent the last few years building up an immunity to iocane powder."')
